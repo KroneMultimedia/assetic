fa61bd9d6f993e40c2a1e52f9efcbb3f88eed39e
devssc <34648756+devssc@users.noreply.github.com>
2025-08-29 10:43:26 +0200

    Update StringAsset.php

--- src/Assetic/Asset/StringAsset.php
+++ src/Assetic/Asset/StringAsset.php
@@ -38,7 +38,7 @@ class StringAsset extends BaseAsset
         parent::__construct($filters, $sourceRoot, $sourcePath);
     }
 
-    public function load(FilterInterface $additionalFilter = null)
+    public function load(?FilterInterface $additionalFilter = null)
     {
         $this->doLoad($this->string, $additionalFilter);
     }
